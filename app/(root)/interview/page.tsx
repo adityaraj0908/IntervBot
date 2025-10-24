f8bf39ae4a79633aa162add219f2e6bdfb100b54
Aditya Raj <adityaraj1389@gmail.com>
2025-10-25 01:30:20 +0530

    Interview page setup

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h3>Inteview Generation</h3>
+    <Agent username='You' userId='user1' type='generate'/>
+    </>
+  )
+}
+
+export default page
